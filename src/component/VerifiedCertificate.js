4159a7c67c3bbbf6d14d70d035caf50f5822f6a1
nikhjha <nikh.jha550@gmail.com>
2022-03-30 07:23:07 +0530

    Making certificate using canvas

--- src/component/VerifiedCertificate.js
+++ src/component/VerifiedCertificate.js
@@ -3,44 +3,70 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 import jsPDF from "jspdf";
 import { UtilityContext } from "../context/UtilityProvider";
-import getFetch from "../libs/axiosClient";
+import getFetch, { frontUrl } from "../libs/axiosClient";
 import { useParams } from "react-router-dom";
+
+//720
+//1470
 
 const certificate = {
   font: "84px serif",
   width: 3088,
   height: 2183,
-  id: {
-    offsetX: 500,
-    offsetY: 400,
+  issueDate: {
+    offsetX: 2725,
+    offsetY: 135,
   },
   name: {
-    offsetX: 500,
-    offsetY: 520,
+    offsetX: 1544,
+    offsetY: 880,
   },
   role: {
-    offsetX: 500,
-    offsetY: 640,
+    offsetX: 1095,
+    offsetY: 1018,
   },
   joiningDate: {
-    offsetX: 500,
-    offsetY: 760,
+    offsetX: 2050,
+    offsetY: 1018,
   },
   endDate: {
-    offsetX: 1500,
-    offsetY: 760,
+    offsetX: 2450,
+    offsetY: 1018,
   },
   link: {
-    offsetX: 2250,
-    offsetY: 1400,
+    offsetX: 600,
+    offsetY: 1350,
   },
 };
 
 const qr = {
-  width: 400,
-  height: 400,
-  offsetX: 2250,
+  width: 300,
+  height: 300,
+  offsetX: 1350,
   offsetY: 1500,
+};
+
+const formatDate = (date) => {
+  if (date !== undefined && date !== "") {
+    var myDate = new Date(date);
+    var month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ][myDate.getMonth()];
+    var str = myDate.getDate() + " " + month + " " + myDate.getFullYear();
+    return str;
+  }
+  return "";
 };
 
 const CreateImage = (src) => {
@@ -56,75 +82,8 @@ const CreateImage = (src) => {
   });
 };
 
-function VerifiedCertificate() {
-  const canvas = useRef();
-  const [user, setUser] = useState(null);
-  const { openLoading, closeLoading } = useContext(UtilityContext);
-  const params = useParams();
-  useEffect(() => {
-    const draw = async () => {
-      const ctx = canvas.current.getContext("2d");
-      ctx.fillStyle = "rgb(255, 255, 255)";
-      ctx.fillRect(0, 0, certificate.width, certificate.height);
-      const img = await CreateImage("/certificate.png");
-      ctx.drawImage(img, 0, 0, certificate.width, certificate.height);
-      const qrcanvas = document.getElementById("qrcanvas");
-      if (qrcanvas.getContext) {
-        const qrData = qrcanvas.toDataURL("image/png");
-        const qrImg = await CreateImage(qrData);
-        ctx.drawImage(qrImg, qr.offsetX, qr.offsetY, qr.width, qr.height);
-      }
-      ctx.font = certificate.font;
-      ctx.fillStyle = "rgb(0,0,0)";
-      ctx.fillText(
-        `Certificate Id : ${user._id}`,
-        certificate.id.offsetX,
-        certificate.id.offsetY
-      );
-      ctx.fillText(
-        `Name : ${user.firstName} ${user.lastName}`,
-        certificate.name.offsetX,
-        certificate.name.offsetY
-      );
-      ctx.fillText(
-        `Role : ${user.role.toUpperCase()}`,
-        certificate.role.offsetX,
-        certificate.role.offsetY
-      );
-      ctx.fillText(
-        `From : ${user.joiningDate}`,
-        certificate.joiningDate.offsetX,
-        certificate.joiningDate.offsetY
-      );
-      ctx.fillText(
-        `To : ${user.endDate}`,
-        certificate.endDate.offsetX,
-        certificate.endDate.offsetY
-      );
-      ctx.fillText(
-        `Link : ${user.link}`,
-        certificate.link.offsetX,
-        certificate.link.offsetY
-      );
-    };
-    if (user) {
-      draw();
-    }
-  }, [user]);
-  useEffect(() => {
-    const fetch = async () => {
-      openLoading();
-      try {
-        const id = params.id;
-        const res = await getFetch().get(`/certificate/${id}`);
-        console.log(res);
-      } catch (e) {
-        console.log(e.response);
-      }
-      closeLoading();
-    };
-    fetch();
-  }, []);
+const SideBox = ({ user, canvas }) => {
+  const link = `${frontUrl}/verify/${user.id}`;
   const generatePdf = () => {
     const options = {
       orientation: "l",
@@ -141,8 +100,114 @@ function VerifiedCertificate() {
       Math.floor(certificate.height / 5),
       "NONE"
     );
-    pdf.save();
+    pdf.save(`${user.firstName}_internship_completion`);
   };
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        height: "100%",
+        borderRadius: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        p: 4,
+        gap: 4,
+      }}
+    >
+      <Typography variant="subtitle1">
+        Link : &nbsp;{" "}
+        <Typography
+          variant="subtitle1"
+          variantMapping={{ subtitle1: "a" }}
+          href={link}
+        >
+          {link}
+        </Typography>
+      </Typography>
+      <QRCodeCanvas value={link} id="qrcanvas" />
+      <Button variant="contained" onClick={generatePdf}>
+        Download PDF
+      </Button>
+    </Box>
+  );
+};
+
+function VerifiedCertificate() {
+  const canvas = useRef();
+  const [user, setUser] = useState(null);
+  const { openLoading, closeLoading } = useContext(UtilityContext);
+  const params = useParams();
+  useEffect(() => {
+    const draw = async () => {
+      const ctx = canvas.current.getContext("2d");
+      ctx.fillStyle = "rgb(255, 255, 255)";
+      ctx.fillRect(0, 0, certificate.width, certificate.height);
+      const img = await CreateImage("/certificate.jpg");
+      ctx.drawImage(img, 0, 0, certificate.width, certificate.height);
+      const qrcanvas = document.getElementById("qrcanvas");
+      if (qrcanvas.getContext) {
+        const qrData = qrcanvas.toDataURL("image/png");
+        const qrImg = await CreateImage(qrData);
+        ctx.drawImage(qrImg, qr.offsetX, qr.offsetY, qr.width, qr.height);
+      }
+      ctx.font = "64px sarif";
+      ctx.fillStyle = "rgb(0,0,0)";
+      ctx.fillText(
+        formatDate(user.issueDate),
+        certificate.issueDate.offsetX,
+        certificate.issueDate.offsetY
+      );
+      const name = `${user.firstName} ${user.lastName}`;
+      const link = `${frontUrl}/verify/${user.id}`;
+      const startDate = formatDate(user.startDate);
+      const endDate = formatDate(user.endDate);
+      ctx.font = "italic bold 124px sarif";
+      ctx.fillText(
+        name,
+        certificate.name.offsetX - Math.floor((name.length * 124) / 4),
+        certificate.name.offsetY
+      );
+      ctx.font = "bold 45px sarif";
+      const role = user.role.toUpperCase();
+      ctx.fillText(
+        role,
+        certificate.role.offsetX - Math.floor((role.length * 45) / 3),
+        certificate.role.offsetY
+      );
+      ctx.font = "bold 54px sarif";
+      ctx.fillText(
+        startDate,
+        certificate.joiningDate.offsetX,
+        certificate.joiningDate.offsetY
+      );
+      ctx.fillText(
+        endDate,
+        certificate.endDate.offsetX,
+        certificate.endDate.offsetY
+      );
+      ctx.font = "64px serif";
+      ctx.fillText(link, certificate.link.offsetX, certificate.link.offsetY);
+    };
+    if (user) {
+      draw();
+    }
+  }, [user]);
+  useEffect(() => {
+    const fetch = async () => {
+      openLoading();
+      try {
+        const id = params.id;
+        const res = await getFetch().get(`/certificate/${id}`);
+        console.log(res);
+        setUser(res.data.certificate);
+      } catch (e) {
+        console.log(e.response);
+      }
+      closeLoading();
+    };
+    fetch();
+  }, []);
   return (
     <Box>
       {user && (
@@ -174,33 +239,7 @@ function VerifiedCertificate() {
               }}
             />
           </Box>
-          <Box
-            sx={{
-              backgroundColor: "white",
-              height: "100%",
-              borderRadius: "1rem",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              p: 4,
-              gap: 4,
-            }}
-          >
-            <Typography variant="subtitle1">
-              Link : &nbsp;{" "}
-              <Typography
-                variant="subtitle1"
-                variantMapping={{ subtitle1: "a" }}
-                href={user.link}
-              >
-                {user.link}
-              </Typography>
-            </Typography>
-            <QRCodeCanvas value={user.link} id="qrcanvas" />
-            <Button variant="contained" onClick={generatePdf}>
-              Download PDF
-            </Button>
-          </Box>
+          <SideBox user={user} canvas={canvas} />
         </Box>
       )}
     </Box>
